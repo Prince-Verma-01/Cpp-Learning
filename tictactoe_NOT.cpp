147d8fd5e3d6248202384a7292dbd521e6a44f83
Prince Verma <princeverma3836@gmail.com>
2025-08-29 23:18:40 +0530

    Update tictactoe_NOT.cpp

--- tictactoe_NOT.cpp
+++ tictactoe_NOT.cpp
@@ -1,29 +1,68 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <limits>
+
+/////////////////////// FUNCTIONS DECLARATION ////////////////////////
 
 void drawBoard(char spaces[]);
 void playerMove(char spaces[], char player );
 void computerMove(char spaces[], char computer);
-void checkWin(char spaces[]);
+bool checkDraw(char spaces[]);
+bool checkWin(char spaces[], char player);
+void clearScreen();
+
+//////////////////////////////////////////////////////////////////////
+
 int main()
 {
+    srand(time(0));
     char spaces[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
     
     int userInput;
     int index;
     std::cout << "********************* TIC TAC TOE *********************\n";
+    drawBoard(spaces);
     char player = 'X';
     char computer = 'O';
     
-drawBoard(spaces);
-playerMove(spaces, player);
-drawBoard(spaces);
-computerMove(spaces, computer);        
-drawBoard(spaces);
-  
-    
+    while(true){
+        playerMove(spaces, player); 
+        if(checkWin(spaces, player)){
+            clearScreen();
+            drawBoard(spaces);
+            std::cout << "You Win :)\n";
+            break;
+        } 
+        else if(checkDraw(spaces)){
+            clearScreen();
+            drawBoard(spaces);
+            std::cout << "It's a Tie\n";
+            break;
+        }
+        
+        computerMove(spaces, computer);
+        clearScreen();
+        drawBoard(spaces);
+        if(checkWin(spaces, computer)){
+            clearScreen();
+            drawBoard(spaces);
+            std::cout << "Computer Wins :(\n";
+            break;
+        }
+        else if(checkDraw(spaces)){
+            clearScreen();
+            drawBoard(spaces);
+            std::cout << "It's a draw\n";
+            break;
+        }
+        
+    }
+
     return 0;
 }
 
+/////////////////////// FUNCTIONS DEFINATIONS ////////////////////////
 
 void drawBoard(char spaces[]){
     std::cout << "     |     |     \n";
@@ -33,7 +72,7 @@ void drawBoard(char spaces[]){
     std::cout << "  "<< spaces[3]<<"  |  "<< spaces[4]<<"  |  "<< spaces[5] <<"  \n";
     std::cout << "_____|_____|_____\n";
     std::cout << "     |     |     \n";
-    std::cout << "  "<< spaces[0]<<"  |  "<< spaces[1]<<"  |  "<< spaces[2] <<"  \n";
+    std::cout << "  "<< spaces[6]<<"  |  "<< spaces[7]<<"  |  "<< spaces[8] <<"  \n";
     std::cout << "     |     |     \n";
 }
 
@@ -44,27 +83,60 @@ void playerMove(char spaces[],char player){
         std::cout << "Enter Your Move (1-9)\n";
         std::cin >> number;
         number--;
-        if(spaces[number] == ' '){
-            spaces[number] = player;
-        }
         
-    }while(!number>= 1 && !number <= 9);
-
+    } while( number < 0 || number > 8 || spaces[number] != ' ');
+    spaces[number] = player;
 }
 
 void computerMove(char spaces[], char computer){
-    srand(time(0));
     int number;
-    do{
-        number = (rand() % 9) + 1;
-        number--;
-        if(spaces[number] == ' '){
-            spaces[number] = computer;
-        }   
-    }while(!number>= 1 && !number <= 9);
     
+    do{
+        number = rand() % 9;    
+    } while(spaces[number] != ' ');
+        spaces[number] = computer;
 }
 
+bool checkWin(char spaces[], char player){
+    // HORIZONTAL
+    if(spaces[0] == player && spaces[1] == player && spaces[2] == player){
+        return true;
+    }
+    if(spaces[3] == player && spaces[4] == player && spaces[5] == player){
+        return true;
+    }
+    if(spaces[6] == player && spaces[7] == player && spaces[8] == player){
+        return true;
+    }
+    
+    // VERTICAL 
+    if(spaces[0] == player && spaces[3] == player && spaces[6] == player){
+        return true;
+    } 
+    if(spaces[1] == player && spaces[4] == player && spaces[7] == player){
+        return true;
+    } 
+    if(spaces[2] == player && spaces[5] == player && spaces[8] == player){
+        return true;
+    }
+    
+    // DIAGONAL 
+    if(spaces[0] == player && spaces[4] == player && spaces[8] == player){
+        return true;
+    }
+    if(spaces[2] == player && spaces[4] == player && spaces[6] == player){
+        return true;
+    } 
+    return false;
+}
 
+bool checkDraw(char spaces[]){
+    for(int i = 0; i < 9; i++){
+        if(spaces[i] == ' ') return false;
+    }
+    return true;
+}
 
-
+void clearScreen() {
+     for (int i = 0; i < 30; i++) std::cout << "\n";
+}
